723ace996b5a8b24ba4deb4eb883ed9ac068eaaa
joashchn <joashchn@gmail.com>
2019-11-18 17:34:11 +0800

    fix className error

--- data/classback_dataset.py
+++ data/classback_dataset.py
@@ -19,9 +19,8 @@ from PIL import Image
 import random
 
 
-class ClassDataset(BaseDataset):
+class ClassbackDataset(BaseDataset):
     """A template dataset class for you to implement custom datasets."""
-
     @staticmethod
     def modify_commandline_options(parser, is_train):
         """Add new dataset-specific options, and rewrite default values for existing options.
@@ -54,6 +53,14 @@ class ClassDataset(BaseDataset):
         self.data_dir = opt.dataroot
         self.dir_train = os.path.join(opt.dataroot, 'train')
         self.dir_val = os.path.join(opt.dataroot, 'val')
+        self.train_path_A = sorted(make_dataset(self.dir_train+'/ants', opt.max_dataset_size))
+        self.train_path_B = sorted(make_dataset(self.dir_train+'/bees', opt.max_dataset_size))
+        self.val_path_A = sorted(make_dataset(self.dir_val+'/ants', opt.max_dataset_size))
+        self.val_path_B = sorted(make_dataset(self.dir_val+'/bees', opt.max_dataset_size))
+        self.train_A_size = len(self.train_path_A)
+        self.train_B_size = len(self.train_path_B)
+        self.val_A_size = len(self.val_path_A)
+        self.val_B_size = len(self.val_path_B)
 
         # define the default transform function. You can use <base_dataset.get_transform>; You can also define your custom transform function
         if opt.model_name == 'inception':
@@ -61,11 +68,6 @@ class ClassDataset(BaseDataset):
         else:
             opt.crop_size = 224
         self.transform = get_transform(opt)
-
-        self.train_sets = datasets.ImageFolder(self.dir_train, transform=self.transform)
-        self.val_sets = datasets.ImageFolder(self.dir_val, transform=self.transform)
-        self.train_size = len(self.train_sets)
-        self.val_size = len(self.val_sets)
 
     def __getitem__(self, index):
         """Return a data point and its metadata information.
@@ -81,11 +83,28 @@ class ClassDataset(BaseDataset):
         Step 3: convert your data to a PyTorch tensor. You can use helpder functions such as self.transform. e.g., data = self.transform(image)
         Step 4: return a data point as a dictionary.
         """
-        train_data = self.train_sets[index % self.train_size]
-        val_data = self.val_sets[index % self.val_size]
-        return {'train': train_data,
-                'val': val_data}
+        # self.dataset_dic = {x: datasets.ImageFolder(os.path.join(self.data_dir, x), self.transform[x]) for x in
+        #                   ['train', 'val']}
+        #
+        # return self.dataset_dic
+
+        train_path_B = self.train_path_B[index % self.train_B_size]
+        train_path_A = self.train_path_A[index % self.train_A_size]
+        val_path_A = self.val_path_A[index % self.val_A_size]
+        val_path_B = self.val_path_B[index % self.val_B_size]
+
+        train_A_img = Image.open(train_path_A).convert('RGB')
+        train_B_img = Image.open(train_path_B).convert('RGB')
+        val_A_img = Image.open(val_path_A).convert('RGB')
+        val_B_img = Image.open(val_path_B).convert('RGB')
+
+        train_A = self.transform(train_A_img)
+        train_B = self.transform(train_B_img)
+        val_A = self.transform(val_A_img)
+        val_B = self.transform(val_B_img)
+
+        return {'train_A': train_A, 'train_B': train_B, 'val_A': val_A, 'val_B': val_B}
 
     def __len__(self):
         """Return the total number of images."""
-        return self.train_size
+        return self.train_A_size
