32b0e7b455bd072e84a06246173e50b2b8812f20
joashchn <joashchn@gmail.com>
2019-11-20 13:03:25 +0800

    add visualize in test process

--- util/visualizer.py
+++ util/visualizer.py
@@ -179,7 +179,7 @@ class Visualizer():
 
         if self.display_id > 0:
 
-            self.vis.image(img, win=display_id, opts={'title': 'class:%s' % display_id})
+            self.vis.image(img, win=display_id, opts={'title': 'class:%s' % label})
             # self.vis.text(label, win=display_id+1)
 
     def plot_current_losses(self, epoch, counter_ratio, losses):
